183a9f328fc689f02f4090df4ddf770e2132ccc2
Neil Gall <neil.gall@symphonicsoft.com>
2018-06-16 14:52:02 +0100

    Implement REST

--- app/engine/src/main/kotlin/uk/neilgall/rulesapp/REST.kt
+++ app/engine/src/main/kotlin/uk/neilgall/rulesapp/REST.kt
@@ -1,5 +1,26 @@
 package uk.neilgall.rulesapp
 
-fun doREST(url: String, method: RESTMethod, params: Map<String, Value>): String {
-    return "ok"
+import com.github.kittinunf.fuel.Fuel
+
+typealias Params = List<Pair<String, String>>
+
+private fun get(url: String) =
+        Fuel.get(url).responseString().third.get()
+
+private fun post(url: String, params: Params) =
+        Fuel.post(url, params).responseString().third.get()
+
+private fun put(url: String, params: Params) =
+        Fuel.put(url, params).responseString().third.get()
+
+private fun delete(url: String, params: Params) =
+        Fuel.delete(url, params).responseString().third.get()
+
+private fun Map<String, Value>.format(): Params = map { Pair(it.key, it.value.toString()) }
+
+fun doREST(url: String, method: RESTMethod, params: Map<String, Value>): String = when(method) {
+    RESTMethod.GET -> get(url)
+    RESTMethod.POST -> post(url, params.format())
+    RESTMethod.PUT -> put(url, params.format())
+    RESTMethod.DELETE -> delete(url, params.format())
 }
