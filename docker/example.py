6c8ac50cecbf5f0d4d37dd838ed23355e3475c23
Neil Gall <neil.gall@symphonicsoft.com>
2018-07-18 09:47:17 +0100

    Add py-docker example

---
+++ docker/example.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import docker as dockerlib
+import time
+
+docker = dockerlib.from_env()
+
+class DockerContainer:
+    def __init__(self, image_name):
+        self._image_name = image_name
+
+    def __enter__(self):
+        self._container = docker.containers.run(image=self._image_name, detach=True)
+        return self._container
+
+    def __exit__(self, *args):
+        self._container.kill()
+        self._container.remove()
+
+if __name__ == '__main__':
+    with DockerContainer("rulesapp-compiler") as compiler:
+        try:
+            for line in compiler.logs(stream=True):
+                print(">> %s" % line)
+        except InterruptedError as e:
+            pass
+
+
